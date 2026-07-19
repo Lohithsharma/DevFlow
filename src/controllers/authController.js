7a6b42ddc2511190eaebfa43fbcf89c91dd98189
Lohith <lohithsarma2006@gmail.com>
2026-07-19 19:16:46 +0530

    feat:Created Login API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const userModel=require('../models/user.model')
 const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
 async function registerUser(req,res){
     try{
@@ -41,6 +42,56 @@ async function registerUser(req,res){
     }
 }
 
+async function loginUser(req,res){
+    try{
+        const {email,password}=req.body
+        if(!email || !password){
+            return res.status(400).json({
+                message:"All fields are required"
+            })
+        }
+
+        const user=await userModel.findOne({email}).select("+password")
+
+        if(!user){
+            return res.status(401).json({
+                message:"Invalid email or password"
+            })
+        }
+
+        const isPasswordCorrect=await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect){
+            return res.status(401).json({
+                message:"Invalid email or password"
+            })
+        }
+        const token=jwt.sign(
+            {
+                id:user._id,
+                role:user.role
+            },
+            process.env.JWT_SECRET
+        )
+
+        return res.status(200).json({
+            message:"User Logged in Successfully",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role
+            }
+        })
+    }catch(error){
+        console.log(error.message)
+        return res.status(500).json({
+            message:"Internal Server Error"
+        })
+    }
+}
+
 module.exports={
-    registerUser
+    registerUser,
+    loginUser
 }
